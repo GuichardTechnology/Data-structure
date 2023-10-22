2c0d63fcae61fa8d313a0895adff0e5ff8a2ebca
GuichardTechnology <guichardj483@gmail.com>
2023-10-22 16:23:22 -0300

    exercices

---
+++ Script.js
@@ -0,0 +1,108 @@
+/*const pays = [
+    "Haiti",
+    "Argentine",
+    "Peru",
+    "Canada",
+    "France",
+    "Usa"
+];
+
+function loopFor() {
+    debugger;
+    for (let i = 0; i < pays.length; i++) {
+        console.log(pays[i])
+    }
+}
+loopFor();*/
+
+
+/*const alphabets = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f"
+
+];
+
+
+function forOf() {
+    debugger;
+
+    for (element of alphabets){
+        console.log(element);
+    }
+}
+
+forOf();*/
+
+/*const numbers = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6
+];
+
+function forEach(){
+    debugger
+      
+    numbers.forEach((element) =>{
+      console.log(element);
+
+    });
+}
+
+forEach()*/
+
+
+/*const pays = [
+
+    "Haiti",
+    "Argentine",
+    "Peru",
+    "Canada",
+    "France",
+    "Usa"
+]
+
+function map() {
+
+    debugger;
+    const array = pays.map((element) => element);
+    console.log(array);
+}
+
+map();*/
+
+
+const pays = [
+
+    "Haiti",
+    "Argentine",
+    "Peru",
+    "Canada",
+    "France",
+    "Usa"
+]
+
+function loopwhile(){
+
+    debugger;
+    const payscount = pays.length;
+    let i = 0;
+    while(i < payscount){
+        console.log(pays[i]);i++
+    }
+}
+
+loopwhile();
+
+
+
+
+
+
+
